c54868789d4ad928576d39f212ebf21b3d544021
smpastushok-ui <smpastushok@gmail.com>
2026-04-19 23:15:23 +0300

    fix(vite): resolve merge conflict marker from prior stash pop

--- packages/client/android/app/src/main/java/app/nebulife/game/MainActivity.java
+++ packages/client/android/app/src/main/java/app/nebulife/game/MainActivity.java
@@ -1,5 +1,13 @@
 package app.nebulife.game;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(GoogleAuth.class);
+    }
+}
